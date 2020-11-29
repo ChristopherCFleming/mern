93fa4f67c1c2a1e07a4a03f7af0c3adb5c5d3e65
ChristopherCFleming <chris.charnquist@gmail.com>
2020-11-28 23:30:10 -0500

    redo from the beginning

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const mongoose = require('mongoose');
 const express = require("express");
+const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-const bodyParser = require('body-parser');
-const passport = require('passport');
-require('./config/passport')(passport);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.use(passport.initialize());
-
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
-
+app.get("/", (req, res) => res.send("Hello Baz"));
+  
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const port = process.env.PORT || 5000;
+  
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
